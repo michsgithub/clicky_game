3d5fdc287d2dd54a771dea12cb0f7a0c41a010a4
michsgithub <michen914@yahoo.com>
2019-01-30 08:24:34 -0800

    Updates

---
+++ precache-manifest.e9a9962afc0532f13017bffbefd84479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b494bb0be0c4d10451c8",
+    "url": "/clicky_game/static/css/main.6ded998f.chunk.css"
+  },
+  {
+    "revision": "b494bb0be0c4d10451c8",
+    "url": "/clicky_game/static/js/main.b494bb0b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/clicky_game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bd845ee937b9e5f2a5a2",
+    "url": "/clicky_game/static/js/runtime~main.bd845ee9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky_game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6a3a766ba8dd51768d8996bf09fc8630",
+    "url": "/clicky_game/index.html"
+  }
+];
